5378309d07c2f21a3f6cd9cd0975da3d7d2c36d1
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-11-16 20:23:43 +0700

    chore: add hmr time

--- src/visit-puppeteer.js
+++ src/visit-puppeteer.js
@@ -11,22 +11,26 @@ exports.visit = async function visit({ count, url, headless }) {
   const arr = [];
   for (let i = 0; i < count; i++) {
     arr.push(i + 1);
-  }
+	}
+	const hrstart = process.hrtime();
 
   // Using for - of, for better async-await support
   for (const idx of arr) {
-    const spinner = ora(`${idx}. Visiting page ${url}...`).start();
+    const spinner = ora(`${idx}. Visiting page ${url}`).start();
     const { browser, browserPage } = await createBrowser(headless);
     try {
       await browserPage.goto(url, { waitUntil: 'networkidle2' });
       await browserPage.evaluate(() => {});
-			spinner.succeed();
+			spinner.succeed(`${idx}. Success visiting ${url}`);
     } catch (error) {
-			spinner.fail();
+			spinner.fail(`${idx}. Failed visiting ${url}`);
 			console.error(chalk.red(`💥 We got error: `), error);
 		}
     await browser.close();
 	}
 
-	console.info(chalk.greenBright(`\n🤩 Finish visiting the page!`));
+	console.info(chalk.greenBright(`\n🤩 Finish visiting the page!\n`));
+
+	const hrend = process.hrtime(hrstart);
+	console.info(chalk.blueBright(`Total execution time: ${hrend[0]}s`));
 };
